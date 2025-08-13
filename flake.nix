90adff252d4e26fc11d3f775f3d6c04add324d2e
Charles Vin <26749794+CharlesAttend@users.noreply.github.com>
2025-08-13 21:29:57 +0200

    feat: secureboot

--- flake.nix
+++ flake.nix
@@ -12,17 +12,35 @@
     };
   };
 
-  outputs = { self, nixpkgs, lanzaboote, ...}: {
+  outputs = { self, nixpkgs, lanzaboote, ...}@inputs: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-
+        specialArgs = { inherit inputs; };
         modules = [
           # This is not a complete NixOS configuration and you need to reference
           # your normal configuration here.
           ./configuration.nix
 
-          
+          lanzaboote.nixosModules.lanzaboote
+          ({ pkgs, lib, ... }: {
+
+            environment.systemPackages = [
+              # For debugging and troubleshooting Secure Boot.
+              pkgs.sbctl
+            ];
+
+            # Lanzaboote currently replaces the systemd-boot module.
+            # This setting is usually set to true in configuration.nix
+            # generated at installation time. So we force it to false
+            # for now.
+            boot.loader.systemd-boot.enable = lib.mkForce false;
+
+            boot.lanzaboote = {
+              enable = true;
+              pkiBundle = "/var/lib/sbctl";
+            };
+          })
         ];
       };
     };
