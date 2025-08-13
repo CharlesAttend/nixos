6bce855ebe50bf157ff09789355ef5e32a8166ad
Charles Vin <26749794+CharlesAttend@users.noreply.github.com>
2025-08-13 20:45:50 +0200

    feat: flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A SecureBoot-enabled NixOS configurations";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote/v0.4.2";
+
+      # Optional but recommended to limit the size of your system closure.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, lanzaboote, ...}: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        modules = [
+          # This is not a complete NixOS configuration and you need to reference
+          # your normal configuration here.
+          ./configuration.nix
+
+          
+        ];
+      };
+    };
+  };
+}
