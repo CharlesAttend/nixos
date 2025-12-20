9abf0594002a94bfc5608286d5af2334abfdfa5a
Charles Vin <26749794+CharlesAttend@users.noreply.github.com>
2025-12-20 12:56:42 +0100

    feat: switch to limine boot loader

--- flake.nix
+++ flake.nix
@@ -4,15 +4,15 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.2";
+    # lanzaboote = {
+    #   url = "github:nix-community/lanzaboote/v0.4.2";
 
-      # Optional but recommended to limit the size of your system closure.
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    #   # Optional but recommended to limit the size of your system closure.
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
-  outputs = { self, nixpkgs, lanzaboote, ...}@inputs: {
+  outputs = { self, nixpkgs, ...}@inputs: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -22,19 +22,21 @@
           # your normal configuration here.
           ./configuration.nix
 
-          lanzaboote.nixosModules.lanzaboote
-          ({ pkgs, lib, ... }: {
-            # Lanzaboote currently replaces the systemd-boot module.
-            # This setting is usually set to true in configuration.nix
-            # generated at installation time. So we force it to false
-            # for now.
-            boot.loader.systemd-boot.enable = lib.mkForce false;
+          # lanzaboote.nixosModules.lanzaboote
+          # ({ pkgs, lib, ... }: {
+          #   # Lanzaboote currently replaces the systemd-boot module.
+          #   # This setting is usually set to true in configuration.nix
+          #   # generated at installation time. So we force it to false
+          #   # for now.
+          #   boot.loader.systemd-boot.enable = lib.mkForce false;
 
-            boot.lanzaboote = {
-              enable = true;
-              pkiBundle = "/var/lib/sbctl";
-            };
-          })
+          #   boot.lanzaboote = {
+          #     enable = true;
+          #     pkiBundle = "/var/lib/sbctl";
+          #   };
+          # })
+
+          
         ];
       };
     };
--- system.nix
+++ system.nix
@@ -6,10 +6,37 @@
 
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.loader.grub = {
-    devices = [ "nodev" ];
-    efiSupport = true;
-    useOSProber = true;
+  # boot.loader.grub = {
+  #   devices = [ "nodev" ];
+  #   efiSupport = true;
+  #   useOSProber = true;
+  #   extraEntries = ''
+  #     menuentry "Windows" --class windows --class os {
+  #       insmod part_gpt
+  #       insmod fat
+  #       search --no-floppy --fs-uuid --set=root 770D-77B0
+  #       chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+  #     }
+  #   '';
+  # };
+
+  boot.loader.limine = {
+    enable = true;
+    secureBoot.enable = true;
+    maxGenerations = 3;
+    extraEntries = ''
+      /Windows 10
+        protocol: efi
+        path: guid(0cbe6065-3880-479c-afc7-c0ab673b6ffd):/EFI/Microsoft/Boot/bootmgfw.efi
+    '';
+    style = {
+      wallpapers = [ ];
+      interface = {
+        resolution = "1920x1080";
+        branding = "feur";
+        brandingColor = 6;
+      };
+    };
   };
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
