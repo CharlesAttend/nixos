2f26bbdb0db277bcfb90a80a10093effaa7da49d
Charles Vin <cvin-prestataire@ina.fr>
2026-02-10 21:46:48 +0100

    fix: homeserver reboot

--- homeserver/configuration.nix
+++ homeserver/configuration.nix
@@ -15,7 +15,7 @@
     ./nvidia.nix
   ];
   networking.hostName = "homeserver";
-
+  boot.kernelParams = [ "reboot=a,b,k,c" ];
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
