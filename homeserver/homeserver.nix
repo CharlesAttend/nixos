62181c73364a72c854ebf7b3679bee4d7af010a3
Charles Vin <26749794+CharlesAttend@users.noreply.github.com>
2026-02-10 19:52:43 +0100

    feat: add INA laptop ssh key

--- homeserver/homeserver.nix
+++ homeserver/homeserver.nix
@@ -12,6 +12,7 @@
   services.fail2ban.enable = true;
   users.users.charles.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP7ukSzyvyn8rATCSEQZD2bcoY6+WgI5bwSfL6BqIqWp charles@nixos" # desktop
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKPEwFFe06EAOZWcP2kQkV8ooYf0iHx1GUugZ6AggWW2 cvin-prestataire@ina.fr"
   ];
   console.keyMap = "uk";
 }
