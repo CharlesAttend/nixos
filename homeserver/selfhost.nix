a738fdd64d5f67031d511f377820ed4384142bb6
Charles Vin <cvin-prestataire@ina.fr>
2026-02-16 20:50:58 +0100

    fix: indentation

--- homeserver/selfhost.nix
+++ homeserver/selfhost.nix
@@ -3,14 +3,14 @@
 {
   # Immich
   services.immich = {
-     enable = true;
-     machine-learning.enable = true;
-     port = 2283;
-     secretsFile = "/run/secrets/immich";
-     environment = {
-     };
-     openFirewall = true;
-     mediaLocation = "/mnt/data/immich/media";
+    enable = true;
+    machine-learning.enable = true;
+    port = 2283;
+    secretsFile = "/run/secrets/immich";
+    environment = {
+    };
+    openFirewall = true;
+    mediaLocation = "/mnt/data/immich/media";
   };
   services.immich.accelerationDevices = null; # all devices
   users.users.immich.extraGroups = [ "video" "render" ];
