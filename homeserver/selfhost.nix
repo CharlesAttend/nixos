ae8da59aa1413e36820e59a6423dc7bc124a28d0
Charles Vin <26749794+CharlesAttend@users.noreply.github.com>
2026-02-20 22:13:17 +0100

    feat: homeassistant 🥳

--- homeserver/selfhost.nix
+++ homeserver/selfhost.nix
@@ -7,12 +7,11 @@
     machine-learning.enable = true;
     port = 2283;
     secretsFile = "/run/secrets/immich";
-    environment = {
-    };
     openFirewall = true;
+    host = "0.0.0.0";
     mediaLocation = "/mnt/data/immich/media";
+    accelerationDevices = null; # all devices
   };
-  services.immich.accelerationDevices = null; # all devices
   users.users.immich.extraGroups = [ "video" "render" ];
   users.users.charles.extraGroups = [ "immich" ];
 
@@ -20,17 +19,18 @@
   virtualisation.oci-containers = {
     backend = "docker";
     containers.homeassistant = {
-      volumes = [ "home-assistant:/config" ];
+      volumes = [ "home-assistant:/config" "/run/udev:/run/udev:ro" "/dev:/dev" ];
       environment.TZ = "Europe/Berlin";
       # Note: The image will not be updated on rebuilds, unless the version label changes
       image = "ghcr.io/home-assistant/home-assistant:stable";
       extraOptions = [ 
-        # Use the host network namespace for all sockets
-        #"--network=host"
-        # Pass devices into the container, so Home Assistant can discover and make use of them
-        #"--device=/dev/ttyACM0:/dev/ttyACM0"
+        # Use the host network namespace for all socket
+        "--network=host"
+        "--device-cgroup-rule=c 188:* rmw"
       ];
-      devices = [ "/dev/ttyUSB0:/dev/ttyUSB0" ];
     };
   };
+  networking.firewall.allowedTCPPorts = [
+    8123   
+  ];
 } 
