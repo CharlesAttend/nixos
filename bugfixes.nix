1f5960e9c84c88d59d20c8c80b54066fafdf42f4
Charles Vin <cvin-prestataire@ina.fr>
2026-07-02 23:15:33 +0200

    feat: logseq temp fix

--- bugfixes.nix
+++ bugfixes.nix
@@ -1,6 +1,12 @@
 { config, pkgs, ... }:
-
+let
+  pkgs_logseq = import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/ec0c722e017dfccbb2f66a8aafbe003320266d33.tar.gz";
+    sha256 = "0jws2i94asr1yish76799gmyw51dj98n8badq3snc8prifmsd3a5";
+  }) { system = pkgs.stdenv.hostPlatform.system; };
+in
 {
+  environment.systemPackages = [pkgs_logseq.logseq]; # https://github.com/NixOS/nixpkgs/issues/535206#issuecomment-4818425933
   nixpkgs.config.permittedInsecurePackages = [
     "electron-39.8.10" # logseq electron deprecation https://github.com/NixOS/nixpkgs/issues/528213
   ];
