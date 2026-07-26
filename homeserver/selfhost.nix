d63c4ad18e35e708a06df07e085ddd288da1c62d
Charles Vin <26749794+CharlesAttend@users.noreply.github.com>
2026-07-26 14:04:13 +0200

    wip reverse proxy

--- homeserver/selfhost.nix
+++ homeserver/selfhost.nix
@@ -57,15 +57,64 @@
   services.traefik = {
     enable = true;
     staticConfigOptions = {
+      entryPoints = {
+        web = {
+          address = ":80";
+          asDefault = true;
+          http.redirections.entrypoint = {
+            to = "websecure";
+            scheme = "https";
+          };
+        };
+
+        websecure = {
+          address = ":443";
+          asDefault = true;
+          http.tls.certResolver = "letsencrypt";
+        };
+      };
       certificatesResolvers.letsencrypt.acme = {
         email = "charles.vin@outlook.fr";
         dnschallenge.provider = "cloudflare";
       };
+      api.dashboard = true;
+      api.insecure = true;
+      log = {
+        level = "DEBUG";
+        filePath = "${config.services.traefik.dataDir}/traefik.log";
+        format = "json";
+      };
     };
-    # environmentFiles = sops.secrets.cloudflare-traefik.path;
+
+    dynamicConfigOptions.http = {
+      routers = {
+        hass = {
+          entryPoints = [ "websecure" ];
+          service = "hass";
+          rule = "Host(`home.charles.vin`) || PathPrefix(`/hass`)";
+          tls.certResolver = "letsencrypt";
+        };
+        paperless = {
+          entryPoints = [ "websecure" ];
+          service = "paperless";
+          rule = "PathPrefix(`/documents`)";
+          tls.certResolver = "letsencrypt";
+        };
+      };
+
+      services = {
+        hass.loadBalancer.servers = [ { url = "http://localhost:8123"; } ];
+        paperless.loadBalancer.servers = [
+          { url = "http://localhost:${toString config.services.paperless.port}"; }
+        ];
+      };
+    };
+    environmentFiles = [ config.sops.secrets.cloudflare-traefik.path ];
   };
   networking.firewall.allowedTCPPorts = [
     8123 # Homeassistant
+
+    8080 # Traefik dashboard
     80
     443
   ];
